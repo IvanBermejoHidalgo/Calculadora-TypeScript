81fba0b4cd4c38ec245b66b032a394fb4c56cb81
IvanBermejoHidalgo <ibermejo@elpuig.xeill.net>
2024-10-03 20:21:14 +0200

    FUNCIONA CORRECTAMENTE

--- built/index.js
+++ built/index.js
@@ -1,37 +1,27 @@
 "use strict";
+let results = false;
 function BotonResultado(tecla) {
     let resultado = document.getElementById("resultado");
-    //let borrar = "";
-    /*resultado.value += tecla;
-
-
-    if (tecla === "+") {
-        primerNumero = parseFloat(resultado.value);
-        borrar = tecla;
-        //resultado.value = "";
-    } else if (tecla === "=") {
-        
-    }*/
-    //let primerNumero: number = 0;
-    //let segundoNumero: number = 0;
-    //let display: string = "";
-    //let res = 0;
-    //let lastElement = resultado.value.slice(-1);
     if (tecla === "=") {
         resultado.value = eval(resultado.value);
+        results = true;
     }
     else if (tecla === "ac") {
         resultado.value = "";
+        results = false;
     }
     else if (tecla === "%") {
         let num = parseFloat(resultado.value);
         resultado.value = (num / 10).toString();
+        results = true;
     }
     else if (tecla === "delete") {
-        //lastElement;
-        //resultado.splice()
         resultado.value = resultado.value.slice(0, -1);
-        ;
+        results = false;
+    }
+    else if (results) {
+        resultado.value = tecla;
+        results = false;
     }
     else {
         resultado.value += tecla;
